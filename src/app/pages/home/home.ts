bbd467d796b1485c57227932969b164eaadd5028
Erick Jean <erickjean36@gmail.com>
2026-05-15 19:34:27 -0400

    feat: implement Home component layout with vehicle summary and upgrade alert

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [MatIconModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
