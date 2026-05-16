27ed4c5450849342273b36fe724639cd0e82dd56
Erick Jean <erickjean36@gmail.com>
2026-05-16 00:37:27 -0400

    feat: add vehicle creation page with form and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { VehicleCreate } from './pages/vehicle-create/vehicle-create';
 import { VehicleDetail } from './pages/vehicle-detail/vehicle-detail';
 import { Vehicles } from './pages/vehicles/vehicles';
 
@@ -11,6 +12,10 @@ export const routes: Routes = [
   {
     path: 'veiculos',
     component: Vehicles,
+  },
+  {
+    path: 'veiculos/novo',
+    component: VehicleCreate,
   },
   {
     path: 'veiculos/:id',
---
+++ src/app/pages/vehicle-create/vehicle-create.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-vehicle-create',
+  standalone: true,
+  imports: [FormsModule, MatIconModule, RouterLink],
+  templateUrl: './vehicle-create.html',
+  styleUrl: './vehicle-create.scss',
+})
+export class VehicleCreate {
+  protected readonly previewImages = [
+    'https://images.unsplash.com/photo-1542362567-b07e54358753?auto=format&fit=crop&w=900&q=80',
+    'https://images.unsplash.com/photo-1503376780353-7e6692767b70?auto=format&fit=crop&w=900&q=80',
+    'https://images.unsplash.com/photo-1552519507-da3b142c6e3d?auto=format&fit=crop&w=900&q=80',
+  ];
+}
